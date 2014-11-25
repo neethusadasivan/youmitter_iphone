c21f4257f8756543ffd273e171538434b8684248
HOME <home@Macintosh.local>
2014-11-25 17:20:40 +0530

    Checking in first version

---
+++ youmitter/youmitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  youmitter
+//
+//  Created by HOME on 25/11/14.
+//  Copyright (c) 2014 Ruby Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ youmitter/youmitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  youmitter
+//
+//  Created by HOME on 25/11/14.
+//  Copyright (c) 2014 Ruby Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
